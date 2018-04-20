a43d673cc4ea149fa642fdbcb44dfae4a8ae1dfe
lavishkothari <lavishkothari004@gmail.com>
2018-04-21 02:45:26 +0530

    adding test for SudokuChromosome

---
+++ src/test/java/sudoku/solver/ga/SudokuChromosomeTest.java
@@ -0,0 +1,27 @@
+package sudoku.solver.ga;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import sudoku.core.Sudoku;
+
+public class SudokuChromosomeTest {
+
+	@Test
+	public void test() {
+		Sudoku sudoku = Sudoku.getPartiallyFilledSudokuPuzzle(9);
+		List<Sudoku> sudokuList = SudokuUtilForGeneticAlgorithm.getRandomMayBeInvalidConformingSudokuList(100, sudoku);
+
+		SudokuChromosome.addAndRefreshProbabilityOfSelection(sudokuList);
+
+		double totalProbability = SudokuChromosome.getSudokuChromosomeList()
+				.stream()
+				.mapToDouble(x -> x.getProbabilityOfSelection())
+				.reduce(0.0, (x, y) -> x + y);
+
+		Assert.assertTrue(1.0 - totalProbability < 0.000001);
+	}
+
+}
